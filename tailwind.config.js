f0bf610c1c358a7c1a4e17711beeda9a1329ed0f
muthu <mkumar0802@gmail.com>
2024-09-04 22:29:14 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      ss: "320px",
+      // => @media (min-width: 640px) { ... }
+
+      sm: "375px",
+      sl: "425px",
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     container: {
       padding: {
         DEFAULT: '1rem',
         sm: '2rem',
         lg: '1rem',
-        xl: '1rem',
+        xl: '2rem',
         '2xl': '1rem',
       },
     },
